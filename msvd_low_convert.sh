f5a61c6c6920307d8fe47615dee9b40a5047c793
Yongho Choi <chldydgh4687@gmail.com>
2020-07-09 19:16:55 +0900

    Update msvd_low_convert.sh

--- msvd_low_convert.sh
+++ msvd_low_convert.sh
@@ -7,15 +7,17 @@
 
 echo "Convert MSVD into Low-quality MSVD...................."
 
-msvd_dir=/home/ivcl/video_data/AI_FEATURES/MSVD/YouTubeClips
-move_dir=/home/ivcl/video_data/AI_FEATURES/MSVD_low
+msvd_dir= 'your msvd path folder'
+move_dir= 'new output folder'
 
 for entry in $msvd_dir/*
 do
 	echo "$entry converting..................."
 	filepathname="$entry"
 	filename=`basename $entry.avi`
+	
 	ffmpeg -i $filepathname -c:v libx264 -preset slow -crf:v 40 $move_dir/$filename
+	
 	echo "$move_dir/$filename finish............."
 done
 
